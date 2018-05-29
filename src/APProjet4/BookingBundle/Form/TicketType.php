d73bb44873ab4f75e0e1b63aba9359ad74a26b5a
PICTA\picta <pictapica@gmail.com>
2018-05-29 20:03:57 +0200

    Add constraints to TicketType

--- src/APProjet4/BookingBundle/Form/TicketType.php
+++ src/APProjet4/BookingBundle/Form/TicketType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class TicketType extends AbstractType {
 
@@ -16,24 +18,62 @@ class TicketType extends AbstractType {
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('lastname',    TextType::class, [
-                        'label' => 'Nom :',
+                ->add('lastname', TextType::class, [
+                    'label' => 'Nom :',
+                    'constraints' => [
+                        new Lenght([
+                            'min' => 2,
+                            'max' => 40,
+                            'minMessage' => 'Votre nom doit comporter au moins 2 lettres',
+                            'maxMessage' => 'Votre nom doit comporter moins de 40 lettre'
+                                ]),
+                        new NotBlank([
+                            'message' => 'Ce champ ne peut pas être vide.',
+                                ])
+                    ]
                 ])
-            ->add('firstname',   TextType::class, [
-                        'label' => 'Prénom :',
+                ->add('firstname', TextType::class, [
+                    'label' => 'Prénom :',
+                    'constraints' => [
+                        new Lenght([
+                            'min' => 2,
+                            'max' => 40,
+                            'minMessage' => 'Votre prénom doit comporter au moins 2 lettres',
+                            'maxMessage' => 'Votre prénom doit comporter moins de 40 lettre'
+                                ]),
+                        new NotBlank([
+                            'message' => 'Ce champ ne peut pas être vide.',
+                                ])
+                    ]
                 ])
-            ->add('dateOfBirth', BirthdayType::class, [
-                        'label' => 'Date de naissance : ',
-                        'placeholder' => [
-                            'day' => 'Jour',
-                            'month' => 'Mois',
-                            'year' => 'Année'
-                        ]
+                ->add('dateOfBirth', BirthdayType::class, [
+                    'label' => 'Date de naissance : ',
+                    'placeholder' => [
+                        'day' => 'Jour',
+                        'month' => 'Mois',
+                        'year' => 'Année'
+                    ],
+                    'contraints' => [
+                        newLessThan([
+                            'value' => 'today',
+                            'message' => 'Veuillez vérifier la date de naissance'
+                        ]),
+                        //Validator
+                        new isTooOld([]),
+                    ]
                 ])
-            ->add('country',     CountryType::class, [
-                        'label' => 'Pays :',
-                        'label_attr' => ['class' => 'label_block'],
-                ]);
+                ->add('country', CountryType::class, [
+                    'label' => 'Pays :',
+                    'label_attr' => ['class' => 'label_block'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Ce champ ne peut pas être vide.'
+                                ]),
+                        new NotNull([
+                            'message' => 'Ce champ ne peut pas être vide.'
+                                ]),
+                    ]
+        ]);
     }
 
     /**
