b9bd8c765de2e752fd9a3df241f639f03cdeaa04
PICTA\picta <pictapica@gmail.com>
2018-06-12 20:45:22 +0200

    Modification vues contactDetails, form1 & recap et création paymentConfirmation

--- src/APProjet4/BookingBundle/Repository/TicketRepository.php
+++ src/APProjet4/BookingBundle/Repository/TicketRepository.php
@@ -10,19 +10,30 @@ namespace APProjet4\BookingBundle\Repository;
  */
 class TicketRepository extends \Doctrine\ORM\EntityRepository {
     
-    public static function countTicketsPerDay($orderDate) {
-        $qb = $this->createQueryBuilder('t');
-
-        $qb     ->select('count(t.id)')// On compte le nombre d'id de tickets
-              ->leftJoin('t.booking', 'b') //On lie avec l'entité booking
-               ->where('b.orderDate=:orderdate')//On défini un paramètre  => "orderDate"
-                ->setParameter('orderdate', $orderDate) //On attribut une valeur à ce paramètre 
-                ->andWhere('b.status>=:status') //On défini un second paramètre
-                ->setParameter('status', Booking::STATUS_PAID); // On attribut une valeur à ce paramètre
-        // On retourne les résultats
-        return $qb
-                ->getQuery()
-                ->getResult()
-        ;
+    //To do : ecrire la fonction permettant de récuperer le nombre de tickets vendu pour le jour de la commande ($orderDate)
+    
+    public static function countTicketsforVisitDay($orderDate) {
+//        $qb = $this->createQueryBuilder('t');
+//
+//        $qb     ->select('count(t.id)')// On compte le nombre d'id de tickets
+//                ->leftJoin('t.booking', 'b') //On lie avec l'entité booking
+//                ->where('b.orderDate=:orderdate')//On défini un paramètre  => "orderDate"
+//                ->setParameter('orderdate', $orderDate) //On attribut une valeur à ce paramètre 
+//                ->andWhere('b.status>=:status') //On défini un second paramètre
+//                ->setParameter('status', Booking::STATUS_PAID); // On attribut une valeur à ce paramètre
+//        // On retourne les résultats
+//        return $qb
+//                ->getQuery()
+//                ->getResult()
+//        ;
+//    
+    //getCompleteDays($event)
+    //->select('date')
+    
+    //doit renvoyer un tableau de dates
+    //On met tout dans attributs Tickets
+    //qui sera un tableau contenant plein de tickets
     }
 }
+//On selectionne les dates pour lesquelles il y a des bookings de status_Paid et pour chaque date on compte le nombre d'id de tickets
+//si le nb de ticket est > à 1000 on retourne la date (un tableau)
