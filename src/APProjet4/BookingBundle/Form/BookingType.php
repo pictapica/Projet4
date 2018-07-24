9fa3e53ee9b7a5994cb70c0b819a3a612b12d6f3
PICTA\picta <pictapica@gmail.com>
2018-07-24 11:32:46 +0200

    Modif BookinType et TicketType -> unused

--- src/APProjet4/BookingBundle/Form/BookingType.php
+++ src/APProjet4/BookingBundle/Form/BookingType.php
@@ -1,43 +1,43 @@
-<?php
+<?php 
 
-namespace APProjet4\BookingBundle\Form;
-
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class BookingType extends AbstractType
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-                ->add('tickets', CollectionType::class, array(
-                    'entry_type' => TicketType::class,
-                    'allow_add'    => true,
-                    'allow_delete' => true,
-                    'label' => false
-                        ));
-    }/**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'APProjet4\BookingBundle\Entity\Booking'
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'approjet4_bookingbundle_booking';
-    }
-
-
-}
+//namespace APProjet4\BookingBundle\Form;
+//
+//use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+//use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\OptionsResolver\OptionsResolver;
+//
+//class BookingType extends AbstractType
+//{
+//    /**
+//     * {@inheritdoc}
+//     */
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder
+//                ->add('tickets', CollectionType::class, array(
+//                    'entry_type' => TicketType::class,
+//                    'allow_add'    => true,
+//                    'allow_delete' => true,
+//                    'label' => false
+//                        ));
+//    }/**
+//     * {@inheritdoc}
+//     */
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults(array(
+//            'data_class' => 'APProjet4\BookingBundle\Entity\Booking'
+//        ));
+//    }
+//
+//    /**
+//     * {@inheritdoc}
+//     */
+//    public function getBlockPrefix()
+//    {
+//        return 'approjet4_bookingbundle_booking';
+//    }
+//
+//
+//}
