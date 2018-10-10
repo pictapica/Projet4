28bf9009824fd97b2c8cf0fcecfee6f031675416
PICTA\picta <pictapica@gmail.com>
2018-10-10 16:06:09 +0200

    New  fonctional test

--- tests/APProjet4/BookingBundle/Controller/HomeControllerTest.php
+++ tests/APProjet4/BookingBundle/Controller/HomeControllerTest.php
@@ -24,8 +24,18 @@ class HomeControllerTest extends WebTestCase
     
     public function testIndexPageIsup()
     {
-        $this ->client->request('GET', 'events');
+       
         
-        $this->assertSame(500, $this->client->getResponse()->getStatusCode());
+//        $repo= $this->getMockBuilder('APProjet4\BookingBundle\Repository\EventRepository')
+//            ->disableOriginalConstructor()
+//            ->getMock();
+//        
+//        $repo
+//            ->method('findAll')
+//            ->willReturn([]);
+        
+        $this ->client->request('GET', '/events');
+        
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     }
 }
